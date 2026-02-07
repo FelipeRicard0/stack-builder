6febe77290105ed0e3466e639ab12ad9665bd48c
Felipe Ricardo <felipepradopc@gmail.com>
2026-02-06 23:00:12 -0300

    v1.1.0

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,19 @@ import { ThemeProvider } from "./components/theme-provider.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n.ts";
+import ReactGA from "react-ga4";
+import Analytics from "./Analytics.tsx";
+import Clarity from "@microsoft/clarity";
+
+ReactGA.initialize(import.meta.env.VITE_GA_ID);
+Clarity.init(import.meta.env.VITE_CLARITY_ID);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <I18nextProvider i18n={i18n}>
       <ThemeProvider defaultTheme="dark" storageKey="stack-builder-theme">
         <BrowserRouter>
+          <Analytics />
           <App />
         </BrowserRouter>
       </ThemeProvider>
